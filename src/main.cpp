8d15b2aa4837c4dba4f0ee5b80cea4246dc4910d
vanitasis <alandanrian77@gmail.com>
2026-02-24 17:16:43 +0700

    make celcius function

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /*
  * Temperature Checker
- * Copyright (c) 2026 Mie
+ * Copyright (c) 2026 vanitasis
  * Licensed under the MIT License.
  */
 
@@ -11,6 +11,7 @@ using namespace std ;
 
 int opsi; 
 int opsicelcius ;
+char confirm ;
 
 class Konversi {
 
@@ -24,23 +25,22 @@ class Konversi {
         int Dkelvin ;
     };
 
-    void tempimage() {
-        cout << "+=========================================+" << endl;
+    void input() {
+        
+         cout << "+=========================================+" << endl;
         cout << "||  ********* ****** ***   *** *******   ||" << endl;
         cout << "||  ********* ***    **** **** ***   **  ||" << endl;
         cout << "||     ***    ****** *** * *** *******   ||" << endl;
         cout << "||     ***    ***    ***   *** ***       ||" << endl;
         cout << "||  ********* ****** ***   *** ***       ||" << endl;
         cout << "+=========================================+" << endl;
-    }
 
-    void input() {
-        
         cout<< "CHOSE OPERATION : " << endl ;
         cout << "1.CELCIUS" << endl ;
         cout << "2.FAHRENHEIT" << endl ;
         cout << "3.REAMUR" << endl ;
         cout << "4.KELVIN" << endl ;
+        cout << "5.BACK" << endl ;
         cin >> opsi ;
 
     }
@@ -56,16 +56,64 @@ class Konversi {
         
         temperatur temp ;
 
-        cout << "masukan derajat celcius : " ;
-        cin >> temp.Dcelcius ;
+        if (opsicelcius == 1)
+        {
+            cout << "Enter degrees Celsius: " ;
+            cin >> temp.Dcelcius ;
 
+            cout << "result : " 
+                 << temp.Dcelcius * 9 / 5 + 32 
+                 << "° Fahrenheit"  << endl ;
+        }
+
+        cout << "continue ? y/n : " ;
+        cin >> confirm ;
+
+    while (confirm == 'y')
+    {
+    cout << "=====================" << endl ;
+        cout << "CHOSE OPERATION : " << endl ;
+        cout << "1.FAHRENHEIT" << endl ;
+        cout << "2.REAMUR" << endl ;
+        cout << "3.KELVIN" << endl ;
+        cin >> opsicelcius ;
+        
+        temperatur temp ;
+
+        if (opsicelcius == 1)
+        {
+            cout << "Enter degrees Celsius: " ;
+            cin >> temp.Dcelcius ;
+
+            cout << "result : " 
+                 << temp.Dcelcius * 9 / 5 + 32 
+                 << "° Fahrenheit"  << endl ;
+        }
+
+        cout << "continue ? y/n : " ;
+        cin >> confirm ;
+    }
+
+    if (confirm == 'n')
+    {
+        input(); 
+    }
+        
     }
     
 };
 
 int main() {
     Konversi convers ;
-    convers.tempimage() ;
+    convers.input() ;
+
+    if (opsi == 1)
+    {
+        convers.Celcius() ;
+    }else if (opsi == 5) {
+        system("clear") ;
+    }
+    
     
     return 0 ;
 }
