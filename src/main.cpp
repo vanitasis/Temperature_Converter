b8bd77d6545ca8184eba7ae608aa96732d450c33
vanitasis <alandanrian77@gmail.com>
2026-02-27 14:48:56 +0700

    template update

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,40 @@
 
 using namespace std ;
 
+class convertermenu {
+public :
+
+struct {
+    int opsitemp ;
+};
+
+void input() {
+        
+        cout << "+=========================================+" << endl;
+        cout << "||  ********* ****** ***   *** *******   ||" << endl;
+        cout << "||  ********* ***    **** **** ***   **  ||" << endl;
+        cout << "||     ***    ****** *** * *** *******   ||" << endl;
+        cout << "||     ***    ***    ***   *** ***       ||" << endl;
+        cout << "||  ********* ****** ***   *** ***       ||" << endl;
+        cout << "+=========================================+" << endl;
+
+        cout<< "CHOSE OPERATION : " << endl ;
+        cout << "1.CELCIUS" << endl ;
+        cout << "2.FAHRENHEIT" << endl ;
+        cout << "3.REAMUR" << endl ;
+        cout << "4.KELVIN" << endl ;
+        cout << "5.BACK" << endl ;
+        cin >> opsitemp ;
+
+    }
+
+
+};
+
 int main() {
+
+    convertermenu temp ;
+    temp.input() ;
 
     double value;
     cout << "Enter Celsius: ";
